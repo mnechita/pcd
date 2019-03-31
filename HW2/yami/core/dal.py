3f980f65d539f76d7de6d0f5d94ccec778bba60d
Mihai Nechita <mihai.nechita95@gmail.com>
2019-03-31 20:11:32 +0300

    fix crypto

--- HW2/yami/core/dal.py
+++ HW2/yami/core/dal.py
@@ -1,22 +1,20 @@
 import boto3
-from yami.settings import *
 from cryptography.fernet import Fernet
+
+from yami.settings import *
 
 
 class KeyGenerator:
+    key = Fernet.generate_key()
+    cipher_suite = Fernet(key)
 
-    def __init__(self):
-        self.key = Fernet.generate_key()
-        self.cipher_suite = Fernet(self.key)
+    @classmethod
+    def encrypt(cls, plain_text):
+        return cls.cipher_suite.encrypt(plain_text)
 
-    def encrypt(self, plain_text):
-        return self.cipher_suite.encrypt(plain_text)
-
-    def decrypt(self, cipher_text):
-        return self.cipher_suite.decrypt(cipher_text)
-
-
-encryption_helper = KeyGenerator()
+    @classmethod
+    def decrypt(cls, cipher_text):
+        return cls.cipher_suite.decrypt(cipher_text)
 
 
 class DAL:
@@ -38,11 +36,11 @@ class DAL:
             'Firstname': user.first_name,
             'Surname': user.last_name,
             'Email': user.email,
-            'Password': encryption_helper.encrypt(bytes(user.password, 'utf-8')),
+            'Password': KeyGenerator.encrypt(bytes(user.password, 'utf-8')),
             'PlayerId': user.username
         })
 
     def check_user(self, user):
-        enc_pass = encryption_helper.encrypt(bytes(user.password, 'utf-8')).decode("utf-8")
+        enc_pass = KeyGenerator.encrypt(bytes(user.password, 'utf-8')).decode("utf-8")
         response = self.users.get_item(TableName='Players', Key={'PlayerId': user.username})
         return response is not None
